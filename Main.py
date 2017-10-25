71081bec88a2ba94ce93c84782cdc6c714111e62
Slavko Bojanic <slavkobojj@gmail.com>
2017-10-25 16:33:00 +1100

    Updated some stuff

--- Main.py
+++ Main.py
@@ -1,16 +1,35 @@
 import pyspeedtest
 import tweepy
 import time
+import datetime
 
+from datetime import date
 from random import randint
 
-def sendTweet(ping, upload, download):
     auth = tweepy.OAuthHandler('consumer_key', 'consumer_secret')
     auth.set_access_token('access_key', 'access_secret')
+lastPassed = datetime.datetime.now()
 
+def sendTweet(ping, upload, download):
     api = tweepy.API(auth)
 
-    api.update_status('@Optus, Why is my download only ' + str(round(download, 2)) + ', and upload only ' + str(round(upload, 2)) + ', when I pay for 100 download and 40 upload? #NBN #Australia #Optus #Broadband')
+    if download < 75:
+        print('Sending negative tweet...')
+        api.update_status('.@Optus, Why am I getting ' + str(round(download, 2)) + 'down/' + str(round(upload, 2)) + 'up, when I pay for 100down/40up in Wollongong, NSW? #NBN #Australia #Optus #Broadband')
+        sleeptime = randint(3000, 6000)
+        m, s = divmod(sleeptime, 60)
+        h, m = divmod(m, 60)
+        print("Checking speeds again in: %d hours %02d minutes and %02d seconds.\n"%(h,m,s))
+        time.sleep(sleeptime)
+    else:
+        print('Sending positive tweet')
+        api.update_status('.@Optus, congrats on having >75 download for the first time since' + str(lastPassed.day) + '/' + str(lastPassed.month) + '/' + str(lastPassed.year))
+        lastPassed = datetime.datetime.now()
+        sleeptime = randint(3000, 6000)
+        m, s = divmod(sleeptime, 60)
+        h, m = divmod(m, 60)
+        print("Checking speeds again in: %d hours %02d minutes and %02d seconds.\n"%(h,m,s))
+        time.sleep(sleeptime)
 
 if __name__ == "__main__":
     while True:
@@ -21,11 +40,4 @@ if __name__ == "__main__":
         upload = upload/1000/1000
         download = st.download()
         download = download/1000/1000
-        if (download < 75):
-            print('Sending tweet...')
-            sendTweet(ping, upload, download)
-            sleeptime = randint(1500, 3000)
-            m, s = divmod(sleeptime, 60)
-            h, m = divmod(m, 60)
-            print("Checking speeds again in: %d hours %02d minutes and %02d seconds.\n"%(h,m,s))
-            time.sleep(sleeptime)
+        sendTweet(ping, upload, download)
